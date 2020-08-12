77bbe69f65a1e924b2fb1332cbb3ae830bd85a4d
Colin Brayton <gringolalia@gmail.com>
2020-08-12 11:45:47 -0300

    deploy

---
+++ precache-manifest.2eafb4491db41b93b751e5b4a80bd4b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fb6f40ac894821080c3",
+    "url": "/css/about.bc42b581.css"
+  },
+  {
+    "revision": "8fb6f40ac894821080c3",
+    "url": "/js/about.9af67c64.js"
+  },
+  {
+    "revision": "0f8dae636e8a3f2feaa1",
+    "url": "/css/app.425f5f02.css"
+  },
+  {
+    "revision": "0f8dae636e8a3f2feaa1",
+    "url": "/js/app.a4dcbfe0.js"
+  },
+  {
+    "revision": "0687fc183459b9a9ed4e",
+    "url": "/css/chunk-vendors.5df73914.css"
+  },
+  {
+    "revision": "0687fc183459b9a9ed4e",
+    "url": "/js/chunk-vendors.cf12d809.js"
+  },
+  {
+    "revision": "7226a46e7f28a60e289da005060e2db6",
+    "url": "/img/enigma.7226a46e.jpg"
+  },
+  {
+    "revision": "f66a01340b59df42f85b034d5a1f1dac",
+    "url": "/img/titannical-steaming.f66a0134.jpg"
+  },
+  {
+    "revision": "984f83e254a6fb20e239490bd11776fb",
+    "url": "/img/manbridge-one.984f83e2.jpg"
+  },
+  {
+    "revision": "bf8afdf2265f192596f96400949d6943",
+    "url": "/img/titannic.bf8afdf2.jpg"
+  },
+  {
+    "revision": "c1af2b92878d29cbe8bdd3bb90df43fc",
+    "url": "/img/manbridge-construct.c1af2b92.jpg"
+  },
+  {
+    "revision": "c7f1a6d37bf3fb28b9668641c6284d36",
+    "url": "/img/nyccourts.c7f1a6d3.jpg"
+  },
+  {
+    "revision": "13d5b92ab2f3b8a5b2675fbb667e7b52",
+    "url": "/img/midtowwnfrown.13d5b92a.jpg"
+  },
+  {
+    "revision": "279dcec949579055f7238a2bf5eb00a4",
+    "url": "/img/vintage-test-two.279dcec9.jpeg"
+  },
+  {
+    "revision": "826ecbb98272a4c582423769ca3eddcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
